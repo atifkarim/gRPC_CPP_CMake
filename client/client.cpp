d4e494215c6b609239e987675ffd4fe346ee534a
atifkarim <atif0902082@gmail.com>
2022-03-13 10:18:34 +0100

    changes to check error handling is done.

--- client/client.cpp
+++ client/client.cpp
@@ -9,42 +9,42 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    // Setup request
-    demo_grpc::C_Request query;
-    demo_grpc::S_Response result;
-    query.set_name("John");
+	// Setup request
+	demo_grpc::C_Request query;
+	demo_grpc::S_Response result;
+	query.set_name("John");
 
-    int32_t x;
-    cout << "Provide the value of cl_x: ";
-    cin >> x;
-    query.set_cl_x(x);
+	int32_t x;
+	cout << "Provide the value of cl_x: ";
+	cin >> x;
+	query.set_cl_x(x);
 
-    // Call
-    auto channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
-    std::unique_ptr<demo_grpc::AddressBook::Stub> stub = demo_grpc::AddressBook::NewStub(channel);
-    grpc::ClientContext context;
-    grpc::Status status = stub->GetAddress(&context, query, &result);
+	// Call
+	auto channel = grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials());
+	std::unique_ptr<demo_grpc::AddressBook::Stub> stub = demo_grpc::AddressBook::NewStub(channel);
+	grpc::ClientContext context;
+	grpc::Status status = stub->GetAddress(&context, query, &result);
 
-    if (status.ok()) {
-        // Output result
-        std::cout << "I got:" << std::endl;
-        std::cout << "Name: " << result.name() << std::endl;
-        std::cout << "City: " << result.city() << std::endl;
-        std::cout << "Zip:  " << result.zip() << std::endl;
-        std::cout << "Street: " << result.street() << std::endl;
-        std::cout << "Country: " << result.country() << std::endl;
-        return 0;
-    } else {
-      // ouch!
-      // lets print the gRPC error message
-      // which is "Length of `Name` cannot be more than 10 characters"
-      std::cout << status.error_message() << std::endl;
-      // lets print the error code, which is 3
-    //   std::cout << status.error_code() << std::endl;
-      // want to do some specific action based on the error?
-    //   if(status.error_code() == StatusCode::INVALID_ARGUMENT) {
-    //     // do your thing here
-    //   }
-      return 0;
-    }
+	if (status.ok()) {
+		// Output result
+		std::cout << "I got:" << std::endl;
+		std::cout << "Name: " << result.name() << std::endl;
+		std::cout << "City: " << result.city() << std::endl;
+		std::cout << "Zip:  " << result.zip() << std::endl;
+		std::cout << "Street: " << result.street() << std::endl;
+		std::cout << "Country: " << result.country() << std::endl;
+		return 0;
+	} else {
+	  // ouch!
+	  // lets print the gRPC error message
+	  // which is "Length of `Name` cannot be more than 10 characters"
+	  std::cout << status.error_message() << std::endl;
+	  // lets print the error code, which is 3
+	//   std::cout << status.error_code() << std::endl;
+	  // want to do some specific action based on the error?
+	//   if(status.error_code() == StatusCode::INVALID_ARGUMENT) {
+	//     // do your thing here
+	//   }
+	  return 0;
+	}
 }
--- server/server.cpp
+++ server/server.cpp
@@ -7,35 +7,43 @@
 #include <iostream>
 using namespace std;
 
+void Check_Value(const ::demo_grpc::C_Request* request)
+{
+	if (request->cl_x() < 5)
+		cout << "Check_Value request->cl_x(): " << request->cl_x() << endl;
+	else
+		throw std::runtime_error("********** BAD VALUE **********");
+}
+
 class AddressBookService final : public demo_grpc::AddressBook::Service {
-    public:
-        virtual ::grpc::Status GetAddress(::grpc::ServerContext* context, const ::demo_grpc::C_Request* request, ::demo_grpc::S_Response* response)
-        {
-            std::cout << "Server: GetAddress for \"" << request->name() << "\"." << std::endl;
+	public:
+		virtual ::grpc::Status GetAddress(::grpc::ServerContext* context, const ::demo_grpc::C_Request* request, ::demo_grpc::S_Response* response)
+		{
+			std::cout << "Server: GetAddress for \"" << request->name() << "\"." << std::endl;
 
-            if (request->cl_x() < 5)
-                cout << "request->cl_x(): " << request->cl_x() << endl;
-            else
-                return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT, "PLEASE set cl_x value less than 5 !!!!!!!!!!");
+			if (request->cl_x() < 5)
+				cout << "request->cl_x(): " << request->cl_x() << endl;
+			else
+				return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT, "PLEASE set cl_x value less than 5 !!!!!!!!!!");
 
-            response->set_name("Peter Peterson");
-            response->set_zip("12345");
-            response->set_country("Superland");
+			response->set_name("Peter Peterson");
+			response->set_zip("12345");
+			response->set_country("Superland");
 
-            return grpc::Status::OK;
-        }
+			return grpc::Status::OK;
+		}
 };
 
 int main(int argc, char* argv[])
 {
-    grpc::ServerBuilder builder;
-    builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
+	grpc::ServerBuilder builder;
+	builder.AddListeningPort("0.0.0.0:50051", grpc::InsecureServerCredentials());
 
-    AddressBookService my_service;
-    builder.RegisterService(&my_service);
+	AddressBookService my_service;
+	builder.RegisterService(&my_service);
 
-    std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
-    server->Wait();
+	std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+	server->Wait();
 
-    return 0;
+	return 0;
 }
